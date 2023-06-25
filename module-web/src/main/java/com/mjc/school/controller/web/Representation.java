31895193671e638d65cef032262a97fab1183dae
Vladimir Haiduk <nightshade2903@gmail.com>
2023-06-25 13:01:29 +0300

    refactoring web layer

--- module-web/src/main/java/com/mjc/school/controller/web/Representation.java
+++ module-web/src/main/java/com/mjc/school/controller/web/Representation.java
@@ -25,18 +25,32 @@ public class Representation {
     }
 
 
-    public void initialProgram(){
-        while(true){
-                startMenu();
-                switch (reader.nextLine()){
-                    case "1" ->  getAllNews();
-                    case "2" -> getNewsById(reader);
-                    case  "3" -> createNews(reader);
-                    case  "4" -> updateNews(reader);
-                    case "5" -> deleteNews(reader);
-                    case "0" -> System.exit(0);
-                    default -> System.out.println(MenuConstants.OPERATION_ERROR);
-                }
+    public void initialProgram() {
+        while (true) {
+            startMenu();
+            switch (reader.nextLine()) {
+                case "1":
+                    getAllNews();
+                    break;
+                case "2":
+                    getNewsById(reader);
+                    break;
+                case "3":
+                    createNews(reader);
+                    break;
+                case "4":
+                    updateNews(reader);
+                    break;
+                case "5":
+                    deleteNews(reader);
+                    break;
+                case "0":
+                    exitProgram();
+                    break;
+                default :
+                    System.out.println(MenuConstants.OPERATION_ERROR);
+                    break;
+            }
         }
     }
 
@@ -44,41 +58,44 @@ public class Representation {
         System.out.println(MenuConstants.START_MENU_TEXT);
     }
 
-    public void getAllNews(){
+    public void getAllNews() {
         System.out.println(MenuConstants.OPERATION_GET_ALL);
         controller.readAllNews().forEach(System.out::println);
     }
 
-    public void getNewsById(Scanner input){
+    public void getNewsById(Scanner input) {
+        System.out.println(MenuConstants.OPERATION_GET_BY_ID);
         System.out.println(MenuConstants.ENTER_ID);
-        try{
+        try {
             System.out.println(controller.getById(inputNumberValidator(input)));
-                    } catch (InvalidDataException | NotFoundException e){
+        } catch (InvalidDataException | NotFoundException e) {
             System.out.println(e.getMessage());
         }
 
 
     }
 
-    public void createNews(Scanner input){
-         newsDto = new NewsDto();
-        try{
+    public void createNews(Scanner input) {
+        System.out.println(MenuConstants.OPERATION_CREATE);
+        newsDto = new NewsDto();
+        try {
             System.out.println(MenuConstants.ENTER_TITLE);
             newsDto.setTitle(input.nextLine());
             System.out.println(MenuConstants.ENTER_CONTENT);
             newsDto.setContent(input.nextLine());
             System.out.println(MenuConstants.ENTER_AUTHOR_ID);
             newsDto.setAuthorId(inputNumberValidator(input));
-            controller.create(newsDto);
-        }catch (InvalidDataException | NotFoundException e) {
+            System.out.println(controller.create(newsDto));
+        } catch (InvalidDataException | NotFoundException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void updateNews(Scanner input){
-         newsDto = new NewsDto();
-        try{
-            System.out.println(MenuConstants.ENTER_AUTHOR_ID);
+    public void updateNews(Scanner input) {
+        System.out.println(MenuConstants.OPERATION_UPDATE);
+        newsDto = new NewsDto();
+        try {
+            System.out.println(MenuConstants.ENTER_ID);
             newsDto.setAuthorId(inputNumberValidator(input));
             System.out.println(MenuConstants.ENTER_TITLE);
             newsDto.setTitle(input.nextLine());
@@ -86,28 +103,33 @@ public class Representation {
             newsDto.setContent(input.nextLine());
             System.out.println(MenuConstants.ENTER_AUTHOR_ID);
             newsDto.setAuthorId(inputNumberValidator(input));
-            controller.update(newsDto);
-        }catch (InvalidDataException | NotFoundException e) {
+            System.out.println(controller.update(newsDto));
+        } catch (InvalidDataException | NotFoundException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void deleteNews(Scanner input){
+    public void deleteNews(Scanner input) {
+        System.out.println(MenuConstants.OPERATION_DELETE);
         System.out.println(MenuConstants.ENTER_ID);
-        try{
+        try {
             System.out.println(controller.delete(inputNumberValidator(input)));
-        } catch (InvalidDataException | NotFoundException e){
+        } catch (InvalidDataException | NotFoundException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    public void exitProgram() {
+        System.out.println(MenuConstants.OPERATION_EXIT);
+        System.exit(0);
+    }
 
 
-    private  Long  inputNumberValidator(Scanner input){
+    private Long inputNumberValidator(Scanner input) {
         Long inputLong = null;
-        try{
+        try {
             inputLong = input.nextLong();
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(MenuConstants.OPERATION_ERROR);
         }
         return inputLong;
